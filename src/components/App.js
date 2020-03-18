de7fbd796db2efd01dc7c4bd8e06d38e07938d8f
Maid Mehic <maid.mehic@compusight.com>
2020-03-18 08:00:03 -0700

    Deleted unnecessary files and folders

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class App extends React.Component {
+
+    render() {
+        return (
+            <div>App works</div>
+        );
+    }
+}
+
+export default App;

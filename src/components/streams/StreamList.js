58a9d13e56370d447d54017614da408b15151a79
Maid Mehic <maid.mehic@compusight.com>
2020-07-13 16:24:50 +0200

    StreamList component implementation

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
 
 class StreamList extends React.Component {
 
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    renderList() {
+        const streams = this.props.streams;
+        return (
+            Object.keys(streams).map(key => {
+                return (
+                    <div className="item" key={key}>
+                        <i className="large film middle aligned icon"></i>
+                        <div className="content">
+                            <a className="header">{streams[key].title}</a>
+                            <div className="description">{streams[key].description}</div>
+                        </div>
+                    </div>
+                );
+            })
+        );
+    }
+
     render() {
         return (
-            <div>StreamList</div>
+            <div className="ui relaxed divided list" style={{ marginBottom: '20px' }}>
+                {
+                    this.renderList()
+                }
+            </div>
         );
     }
 };
 
-export default StreamList;
+const mapStateToProps = state => {
+    return {
+        streams: state.streams
+    };
+}
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -11,7 +11,7 @@ import _ from 'lodash';
 export default (state = {}, action) => {
     switch (action.type) {
         case FETCH_STREAMS:
-            return { ...state, ..._.mapKeys(action.payload, 'id') }; //action.payload returns array, so we take that array and transform it to objects with mapKeys function. Also we merge it with current state and return new object
+            return { ...state, ..._.mapKeys(action.payload, 'id') }; //action.payload returns array, so we take that array and transform it to a object with mapKeys function. Also we merge it with current state and return new object
         case FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case CREATE_STREAM:
